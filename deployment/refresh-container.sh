ed200fa808d2d4c2e7e24ad05fe656f4f7f4c9ec
Kiran <kiran.biswa682@gmail.com>
2025-12-08 18:12:12 -0500

    simplified script

--- deployment/refresh-container.sh
+++ deployment/refresh-container.sh
@@ -4,15 +4,8 @@ CONTAINER_NAME=about-me-site
 
 IMAGE_NAME=kiranrdm/about-me-site:latest
 
-if [ $(sudo docker ps -q -f name=$CONTAINER_NAME) ]; then
-    echo "Stopping container $CONTAINER_NAME..."
-    sudo docker stop $CONTAINER_NAME
-fi
-
-if [ $(sudo docker ps -aq -f name=$CONTAINER_NAME) ]; then
-    echo "Removing container $CONTAINER_NAME..."
-    sudo docker rm $CONTAINER_NAME
-fi
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
 
 echo "Pulling latest Docker image $IMAGE_NAME..."
 sudo docker pull $IMAGE_NAME
